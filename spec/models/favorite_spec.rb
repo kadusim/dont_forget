22f19d2b444d7c2a21a2ffa31d72843620bb21b8
Kadu Simoes <kadu_simoes_1@hotmail.com>
2018-03-22 00:35:58 -0300

    test favorite model

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { create(:user) }
+  let(:list) { create(:list) }
+
+  let(:favorite) { Favorite.new(user_follower_id: user.id, list_followed_id: list.id) }
+
+  it 'is valid with valid attibutes' do
+    expect(favorite).to be_valid
+  end
+
+  it "is not valid without a user id" do
+    favorite.user_follower_id = nil
+    expect(favorite).to_not be_valid
+  end
+
+  it "is not valid without a list id" do
+    favorite.list_followed_id = nil
+    expect(favorite).to_not be_valid
+  end
+
 end
